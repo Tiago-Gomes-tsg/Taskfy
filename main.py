1aba06918b2cca6845b490e1dc3e30770dfb994b
Tiago-Gomes-tsg <tiagogomes71490@gmail.com>
2025-03-30 13:36:06 -0300

    rm main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from task_manager.SetupWindow import mainWindowWidget
-from PySide6.QtWidgets import QApplication
-import sys
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = mainWindowWidget()
-    window.show()
-    sys.exit(app.exec())
